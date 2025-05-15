c01af24861f58c6e1329a7055bc96685fbd36f8c
Lucas Gabriel de Resende <lucasgresende12@gmail.com>
2025-05-15 10:54:23 -0300

    Corrige o tipo de linguagem SQL e corrige o script para que rode todas as migrations

--- config/db.php
+++ config/db.php
@@ -1,23 +1,31 @@
 <?php
 
-$host = 'db';
-$db = 'quickerp-db';
-$user = 'user';
-$pass = 'password';
+$host = 'localhost';
+$db = 'quickerp';
+$user = 'quickerp_user';
+$pass = 'quickerp_user123';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $sql = file_get_contents(__DIR__ . '/sql/V1__create_initial_tables.sql');
-    if($sql == false){
-        throw new Exception('Erro ao ler o arquivo SQL');
-    }
-    $pdo->exec($sql);
+    $sqlFiles = glob(__DIR__ . '/../sql/*.sql');
 
-    echo "Banco de dados criado e migrado com sucesso!";
+    foreach ($sqlFiles as $file) {
+        $filename = basename($file);
+
+        $sql = file_get_contents($file);
+        if ($sql === false) {
+            throw new Exception("Erro ao ler o arquivo $filename");
+        }
+
+        $pdo->exec($sql);
+        echo "Arquivo $filename executado com sucesso.\n";
+    }
+
+    echo "Todas as migrations foram executadas.\n";
 } catch (PDOException $e) {
-    echo "Erro de banco: " . $e->getMessage();
+    echo "Erro de banco: " . $e->getMessage() . "\n";
 } catch (Exception $e) {
-    echo "Erro geral: " . $e->getMessage();
+    echo "Erro geral: " . $e->getMessage() . "\n";
 }
--- sql/V1__create_initial_tables.sql
+++ sql/V1__create_initial_tables.sql
@@ -1,19 +1,19 @@
-CREATE TABLE produtcs (
-    id uuid NOT NULL DEFAULT uuid_generate_v4(),
+CREATE TABLE IF NOT EXISTS produtcs (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name varchar(255) NOT NULL,
     price decimal(10,2) NOT NULL
 );
 
-CREATE TABLE stock (
-    id uuid NOT NULL DEFAULT uuid_generate_v4(),
-    product_id uuid REFERENCES produtcs(id),
+CREATE TABLE IF NOT EXISTS stock (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    product_id INT REFERENCES produtcs(id),
     quantity INT NOT NULL DEFAULT 0,
     FOREIGN KEY (product_id) REFERENCES produtcs(id) ON DELETE CASCADE
 );
 
-CREATE TABLE requests (
-    id uuid NOT NULL DEFAULT uuid_generate_v4(),
-    request_date TIMESTAMP WITH TIME ZONE,
+CREATE TABLE IF NOT EXISTS requests (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    request_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     total_value DECIMAL(10,2),
     freight DECIMAL(10,2),
     zip_code VARCHAR(10),
@@ -21,11 +21,20 @@ CREATE TABLE requests (
     status ENUM('Pending', 'Finished', 'Canceled') DEFAULT 'Pending'
 );
 
-CREATE TABLE coupons (
-    id uuid NOT NULL DEFAULT uuid_generate_v4(),
+CREATE TABLE IF NOT EXISTS coupons (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     code varchar(50) UNIQUE NOT NULL,
     value_discount DECIMAL(10,2),
     minimum_purchase DECIMAL(10,2),
     validate DATE
 );
 
+CREATE TABLE IF NOT EXISTS order_request (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    request_id INT,
+    product_id INT,
+    quantity INT,
+    unitary_price DECIMAL(10,2),
+    FOREIGN KEY (request_id) REFERENCES requests(id) ON DELETE CASCADE,
+    FOREIGN KEY (product_id) REFERENCES produtcs(id)
+);
