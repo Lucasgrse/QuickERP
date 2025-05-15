c63e098df35db930e5479e6fd4589ca6ebdea43c
Lucas Gabriel de Resende <92735087+Lucasgrse@users.noreply.github.com>
2025-05-14 21:34:13 -0300

    Definição das tabelas e inicia o banco através do docker compose

--- config/db.php
+++ config/db.php
@@ -8,6 +8,16 @@ $pass = 'password';
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sql = file_get_contents(__DIR__ . '/sql/V1__create_initial_tables.sql');
+    if($sql == false){
+        throw new Exception('Erro ao ler o arquivo SQL');
+    }
+    $pdo->exec($sql);
+
+    echo "Banco de dados criado e migrado com sucesso!";
 } catch (PDOException $e) {
-    die("Erro na conexão: " . $e->getMessage());
+    echo "Erro de banco: " . $e->getMessage();
+} catch (Exception $e) {
+    echo "Erro geral: " . $e->getMessage();
 }
